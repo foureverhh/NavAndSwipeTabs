09afcc0b999178aac53c358d11261533c5cdb4fd
foureverhh <foureverhh@gmail.com>
2018-11-26 23:04:21 +0100

    Image can get from gallery and show on ImageFragment, but orientation problem not settled

--- app/src/main/java/com/nackademin/foureverhh/navandswipetabs/ImageFragment.java
+++ app/src/main/java/com/nackademin/foureverhh/navandswipetabs/ImageFragment.java
@@ -1,14 +1,26 @@
 package com.nackademin.foureverhh.navandswipetabs;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -16,22 +28,57 @@ import android.widget.TextView;
  */
 public class ImageFragment extends Fragment {
 
-    TextView textView;
+    private ImageView photoFromGallery;
+    private TextView textFromPhotoGallery;
+    private Button buttonGetTextGallery;
+    private static final int GET_PHOTO_FROM_GALLERY = 1;
+
     public ImageFragment() {
         // Required empty public constructor
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        textView = view.findViewById(R.id.image_fragment_text);
-        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_image, container, false);
+        photoFromGallery = (ImageView) rootView.findViewById(R.id.get_photo_from_gallery);
+        textFromPhotoGallery =(TextView) rootView.findViewById(R.id.text_from_photo_gallery);
+        buttonGetTextGallery =(Button) rootView.findViewById(R.id.btn_get_text_photo_gallery);
+        buttonGetTextGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getPhotoFromGallery = new Intent();
+                getPhotoFromGallery.setAction(Intent.ACTION_GET_CONTENT);
+                getPhotoFromGallery.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        ,"image/*");
+                startActivityForResult(
+                        getPhotoFromGallery.createChooser(getPhotoFromGallery,"Select file")
+                        ,GET_PHOTO_FROM_GALLERY);
+            }
+        });
+
+        return rootView;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == GET_PHOTO_FROM_GALLERY && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            try {
+                InputStream inputStream = getActivity()
+                        .getContentResolver()
+                        .openInputStream(imageUri);
+                Bitmap originBitmap = BitmapFactory.decodeStream(inputStream);
+                Bitmap resizedBitmap = ScalePhoto.scaleDownPhoto(originBitmap
+                        , 500
+                        , false);
+                photoFromGallery.setImageBitmap(resizedBitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
---
+++ app/src/main/java/com/nackademin/foureverhh/navandswipetabs/ScalePhoto.java
@@ -0,0 +1,59 @@
+package com.nackademin.foureverhh.navandswipetabs;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
+public class ScalePhoto {
+
+    public static Bitmap scaleDownPhoto(Bitmap sourceBitmap, int desiredPixels, boolean keepOriginBitmap){
+        int width = sourceBitmap.getWidth();
+        int height = sourceBitmap.getHeight();
+        int newWidth = 0, newHeight = 0;
+        float ratio = 0f;
+
+        if(width > height && width > desiredPixels){
+            newWidth = desiredPixels;
+            ratio = (float) newWidth / width;
+            newHeight = (int)(ratio * height);
+        }
+
+        if(width > height && width <= desiredPixels)
+            return sourceBitmap;
+
+        if(width < height && height > desiredPixels){
+            newHeight = desiredPixels;
+            ratio = (float) newHeight / height;
+            newWidth = (int)(ratio * height);
+        }
+
+        if(width < height && height <= desiredPixels)
+            return sourceBitmap;
+
+        if(width == height && width > desiredPixels){
+            newWidth = desiredPixels;
+            newHeight = desiredPixels;
+        }
+
+        if(width == height && height <= desiredPixels)
+            return sourceBitmap;
+
+       return resizedBitmap(sourceBitmap,newWidth,newHeight,keepOriginBitmap);
+
+    }
+
+    public static Bitmap resizedBitmap(Bitmap sourceBitmap,
+                                       int newWidth,
+                                       int newHeight,
+                                       boolean keepOriginBitmap){
+        int width = sourceBitmap.getWidth();
+        int height = sourceBitmap.getHeight();
+        float scaleWidth = (float) newWidth / width;
+        float scaleHeight = (float) newHeight / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth,scaleHeight);
+        Bitmap resizedBitmap = Bitmap.createBitmap(sourceBitmap,0,0,width,height,matrix,false);
+        //if(!keepOriginBitmap)
+        //    resizedBitmap.recycle();
+        return resizedBitmap;
+    }
+}
